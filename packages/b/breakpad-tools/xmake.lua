f19cffa79363f48d64055d42a03fe3c29295b5eb
SanQi <1459998132@.qq.com>
2024-05-31 14:41:37 +0800

    - add breakpad-tools

--- packages/b/breakpad-tools/xmake.lua
+++ packages/b/breakpad-tools/xmake.lua
@@ -5,7 +5,8 @@ package("breakpad-tool")
 
     add_urls("https://github.com/getsentry/breakpad-tools.git")
     add_versions("2020.07.16", "abcafc115e6130c009c3b2efae12ff6ffcc1d539")
-
+    add_patches("2020.07.16","patches/2020.07.16/fixlinux.patch")
+    
     on_install("linux", function(package)
         os.cd("linux")
         local configs = {}
