6b813a668c8bf8837f1c7972abbbc9d6ae4f45c3
sanqideshi <tqw0011@qq.com>
2024-05-31 13:35:37 +0800

    Create xmake.lua

---
+++ packages/b/breakpad-tool/xmake.lua
@@ -0,0 +1,24 @@
+package("crashpad")
+    set_homepage("https://github.com/getsentry/breakpad-tools")
+    set_description("Crashpad is a crash-reporting system.")
+    set_license("Apache-2.0")
+
+    add_urls("https://github.com/getsentry/breakpad-tools.git")
+    add_versions("2020.07.16", "abcafc115e6130c009c3b2efae12ff6ffcc1d539")
+
+    on_install("linux", "windows|x86", "windows|x64", function(package)
+        local configs = {}
+        import("package.tools.cmake").install(package, configs)
+        -- package:addenv("PATH", "bin")
+    end)
+
+    on_test(function(package)
+        if package:is_plat("linux") then
+            os.vrunv("minidump_stackwalk", {"--help"})
+        end
+
+        if package:is_plat("windows") then
+            os.vrunv("minidump_stackwalk.exe", {"--help"})
+        end
+
+    end)
