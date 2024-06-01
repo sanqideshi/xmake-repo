4da8bf3f4467db6855b617114fd93c03ea0408fb
SanQi <1459998132@.qq.com>
2024-06-01 08:11:04 +0800

    edit

--- packages/b/breakpad-tools/xmake.lua
+++ packages/b/breakpad-tools/xmake.lua
@@ -2,29 +2,28 @@ package("breakpad-tools")
     set_homepage("https://github.com/getsentry/breakpad-tools")
     set_description("breakpad tools bins,for example:minidump_stackwalk")
     set_license("Apache-2.0")
-    add_deps("make")
+
 
     add_urls("https://github.com/getsentry/breakpad-tools.git")
-    add_versions("2020.07.16", "abcafc115e6130c009c3b2efae12ff6ffcc1d539")
-    add_patches("2020.07.16","patches/2020.07.16/fixlinux.patch","441c7b5bd07c1d09166b7ee09c4a4b8e6c7d60bac78ffae9d8ff258fd3487a4c")
-    
-    on_install("linux","windows|x86", "windows|x64", function(package)
-        local mplat = package:plat()
-        os.cd(mplat)
-        local makeexec = mplat=="linux" and "make" or "make.bat";
-        os.vrunv(makeexec,{"all"})
-        os.cp("build/*",package:installdir("bin"))
-        package:addenv("PATH", "bin")
+    -- add_versions("2020.07.16", "abcafc115e6130c009c3b2efae12ff6ffcc1d539")
+    -- add_patches("2020.07.16", "patches/2020.07.16/fixlinux.patch","441c7b5bd07c1d09166b7ee09c4a4b8e6c7d60bac78ffae9d8ff258fd3487a4c")
+
+    on_install("linux", "windows|x86", "windows|x64", function(package)
+        -- local mplat = package:plat()
+        -- os.cd(mplat)
+        -- local makeexe = mplat == "linux" and "make" or "make.bat";
+        -- os.vrunv(makeexe, { "all" })
+        -- os.cp("build/*", package:installdir("bin"))
+        -- package:addenv("PATH", "bin")
     end)
 
-    
+
     on_test(function(package)
-        if package:is_plat("linux") then
-            os.vrunv("minidump_stackwalk", {"-help"})
-        end
+        -- if package:is_plat("linux") then
+        --     os.vrunv("minidump_stackwalk", { "-help" })
+        -- end
 
-        if package:is_plat("windows") then
-            os.vrunv("minidump_stackwalk.exe", {"-help"})
-        end
-
+        -- if package:is_plat("windows") then
+        --     os.vrunv("minidump_stackwalk.exe", { "-help" })
+        -- end
     end)
